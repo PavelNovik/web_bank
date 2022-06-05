cc81abae36f2371baa82e87bb13da02e2e7d7798
PavelNovik <brn2befree@gmail.com>
2022-06-05 19:32:46 +0300

    add soft scroll

--- script.js
+++ script.js
@@ -31,3 +31,33 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+// Имплементация плавного скрола
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const section1Coords = section1.getBoundingClientRect();
+  console.log(section1Coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(
+    'Текущее прокручивание: x, y',
+    window.pageXOffset,
+    window.pageYOffset
+  );
+  console.log(
+    'Ширина и высота viewport',
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+  // window.scrollTo(
+  // section1Coords.left + window.pageXOffset,
+  // section1Coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  // left: section1Coords.left + window.pageXOffset,
+  // top: section1Coords.top + window.pageYOffset,
+  // behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
