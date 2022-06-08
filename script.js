222b268d932cee0bf76f91a98c2d9cec36b25647
PavelNovik <brn2befree@gmail.com>
2022-06-08 16:54:54 +0300

    script loading - defer or async, head or endBody

--- script.js
+++ script.js
@@ -339,3 +339,19 @@ dotsContainer.addEventListener('click', function (e) {
     activateCurrentDot(slide);
   }
 });
+
+// Lifecycle DOM Events
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log(e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log(e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
